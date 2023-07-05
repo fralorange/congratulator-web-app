3b1d57a7e19c3ae173f4b1597f1923afacb8dd7e
fralorange <fralorange@mail.ru>
2023-07-05 03:40:53 +0300

    Init basic API for Congratulator App

---
+++ src/congratulator.api/Controllers/CongratulatorController.cs
@@ -0,0 +1,71 @@
+﻿using Congratulator.Api.Entities;
+using Microsoft.AspNetCore.Mvc;
+
+namespace congratulator.api.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class CongratulatorController : ControllerBase
+    {
+        [HttpGet]
+        [Route("api/all")]
+        public BirthdayDateCollection GetBirthdays()
+        {
+            return new BirthdayDateCollection()
+            {
+                Birthdays = new[]
+                {
+                    new BirthdayDate()
+                    {
+                        Id = 1,
+                        FirstName = "Alexander",
+                        LastName = "Makedonsky",
+                        BirthDate = new DateOnly(356, 10, 6)
+                    },
+                    new BirthdayDate()
+                    {
+                        Id = 2,
+                        FirstName = "Alexander",
+                        LastName = "Pushkin",
+                        BirthDate = new DateOnly(1799, 6, 6)
+                    },
+                    new BirthdayDate()
+                    {
+                        Id = 3,
+                        FirstName = "Sergey",
+                        LastName = "Esenin",
+                        BirthDate = new DateOnly(1895, 10, 3)
+                    }
+                }
+            };
+        }
+
+        [HttpGet]
+        [Route("api/coming")]
+        public BirthdayDateCollection GetComingBirthdays()
+        {
+            throw new NotImplementedException();
+        }
+
+        [HttpPost]
+        [Route("api/add")]
+        public IActionResult AddBirthdayDate([FromBody] BirthdayDate birthdayDate)
+        {
+            throw new NotImplementedException();
+        }
+
+        [HttpDelete]
+        [Route("api/delete/{id}")]
+        public IActionResult RemoveBirthdayDate(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        [HttpPut]
+        [Route("api/edit/{id}")]
+        public IActionResult EditBirthdayDate(int id, [FromBody] BirthdayDate editedBirthdayDate)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ src/congratulator.api/Entities/BirthdayDate.cs
@@ -0,0 +1,10 @@
+﻿namespace Congratulator.Api.Entities
+{
+    public class BirthdayDate
+    {
+        public required int Id { get; set; }
+        public required string FirstName { get; set; }
+        public required string LastName { get; set; }
+        public required DateOnly BirthDate { get; set; }
+    }
+}
