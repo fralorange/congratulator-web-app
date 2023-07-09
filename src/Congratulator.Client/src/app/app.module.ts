271abcb5b3bcd24848b3c4de566f558914c519e6
fralorange <fralorange@mail.ru>
2023-07-09 14:32:22 +0300

    Implement sorting features

--- src/Congratulator.Client/src/app/app.module.ts
+++ src/Congratulator.Client/src/app/app.module.ts
@@ -5,6 +5,7 @@ import { FormsModule } from '@angular/forms';
 import { TooltipModule } from 'ngx-bootstrap/tooltip'
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { BsDatepickerModule } from 'ngx-bootstrap/datepicker';
+import { MatSortModule } from '@angular/material/sort';
 
 import { AppComponent } from './app.component';
 import { EditComponent } from './edit/edit.component';
@@ -25,6 +26,7 @@ import { AddComponent } from './add/add.component';
     AppRoutingModule,
     FormsModule,
     BrowserAnimationsModule,
+    MatSortModule,
     TooltipModule.forRoot(),
     BsDatepickerModule.forRoot()
   ],
--- src/Congratulator.Client/src/app/main/main.component.ts
+++ src/Congratulator.Client/src/app/main/main.component.ts
@@ -1,5 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
+import { Sort, MatSortModule } from '@angular/material/sort';
 import { BirthdayDate } from '../core/birthday-date.interface';
 import { BirthdayDateCollection } from '../core/birthday-date-collection.interface';
 
@@ -11,6 +12,7 @@ import { BirthdayDateCollection } from '../core/birthday-date-collection.interfa
 export class MainComponent {
   private readonly _apiUrl = 'Congratulator/api/birthdaydate';
   public birthdayDates?: BirthdayDate[];
+  public sortedDates?: BirthdayDate[];
 
   constructor(private _http: HttpClient) {
     this.loadData(this._apiUrl);
@@ -19,10 +21,35 @@ export class MainComponent {
   public loadData(path: string) {
     this._http.get<BirthdayDateCollection>(path).subscribe(result => {
       this.birthdayDates = result.birthdays;
+      this.sortedDates = this.birthdayDates.slice();
     }, error => console.error(error));
   }
 
-  onCheckboxChange(event: Event) {
+  public sortData(sort: Sort) {
+    const data = this.birthdayDates?.slice();
+    if (!sort.active || sort.direction === '') {
+      this.sortedDates = data;
+      return;
+    }
+
+    this.sortedDates = data?.sort((a, b) => {
+      const isAsc = sort.direction === 'asc';
+      switch (sort.active) {
+        case 'id':
+          return compare(a.id, b.id, isAsc);
+        case 'firstName':
+          return compare(a.firstName, b.firstName, isAsc);
+        case 'lastName':
+          return compare(a.lastName, b.lastName, isAsc);
+        case 'birthDate':
+          return compare(a.birthDate, b.birthDate, isAsc);
+        default:
+          return 0;
+      }
+    });
+  }
+
+  public onCheckboxChange(event: Event) {
     const checked = (event.target as HTMLInputElement).checked;
     let path = this._apiUrl;
 
@@ -33,7 +60,7 @@ export class MainComponent {
     this.loadData(path);
   }
 
-  onButtonClick(id: number) {
+  public onButtonClick(id: number) {
     this._http.delete<void>(`${this._apiUrl}/${id}`).subscribe(() => {
       this.loadData(this._apiUrl);
     },
@@ -42,4 +69,8 @@ export class MainComponent {
         alert("Don't press 'Delete' button so fast");
       });
   }
+}
+
+function compare(a: number | string | Date, b: number | string | Date, isAsc: boolean) {
+  return (a < b ? -1 : 1) * (isAsc ? 1 : -1);
 }
